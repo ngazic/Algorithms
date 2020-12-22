727951633948f335b029a357af55a8881d3cd811
ngazic <ngazic@gmail.com>
2020-12-22 17:04:37 +0100

    5. DFS initial commit

---
+++ Solutions/6.Depth-first-search.js
@@ -0,0 +1,19 @@
+/**
+Depth First Search (DFS) for Graph (Tree is graph)
+
+Two ways to represent Graphs:
+1. Adjacency list
+2. Adjecency matrix
+
+Sample input:
+             A          
+          /  |  \
+        B    C   D
+      /   \    /    \
+    E      F  G     H
+          / \  \
+        I   J    K
+                
+
+Sample output: [A, B, E, F, I, J, C, D, G, K, H]
+ */
