1e9312489dbdda83899dbce538860986b710b6fa
ngazic <ngazic@gmail.com>
2021-01-05 05:05:08 +0100

    16.Run Length Encodging initial commit

---
+++ 16.Run-length-encoding.js
@@ -0,0 +1,5 @@
+/*
+Given an input string, write a function that returns the Run Length Encoded 
+string for the input string. For example, if the input string is 
+“wwwwaaadexxxxxx”, then the function should return “w4a3d1e1x6”.
+*/
