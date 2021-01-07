ef68081f18a35569defc9bb46b8db69a942f94ec
ngazic <ngazic@gmail.com>
2021-01-07 10:43:17 +0100

    17.River Sizes done

--- Solutions/17.River-sizes.js
+++ Solutions/17.River-sizes.js
@@ -29,15 +29,16 @@ Output:
  */
 
 function riverSizes(matrix) {
-  const traversalMatrix = [...Array(matrix.length)].map(() => Array(matrix[0].length).fill(false));
+  const traversalMatrix = [...Array(matrix.length)].map(() =>
+    Array(matrix[0].length).fill(false)
+  );
   const solution = [0];
   for (let i = 0; i < matrix.length; i++) {
     for (let j = 0; j < matrix[i].length; j++) {
       if (traversalMatrix[i][j]) {
         continue;
       }
-      if (solution[solution.length - 1] !== 0) 
-        solution.push(0);
+      if (solution[solution.length - 1] !== 0) solution.push(0);
       followRivers(matrix, traversalMatrix, solution, i, j);
     }
   }
@@ -46,12 +47,9 @@ function riverSizes(matrix) {
 }
 
 function followRivers(input, helper, solution, i = 0, j = 0) {
-  if (i >= helper.length || i < 0) 
-    return 0;
-  if (j >= helper[0].length || j < 0) 
-    return 0;
-  if (helper[i][j]) 
-    return 0;
+  if (i >= helper.length || i < 0) return 0;
+  if (j >= helper[0].length || j < 0) return 0;
+  if (helper[i][j]) return 0;
   helper[i][j] = true;
   if (input[i][j] === 1) {
     solution[solution.length - 1]++;
@@ -64,24 +62,60 @@ function followRivers(input, helper, solution, i = 0, j = 0) {
   }
 }
 
+//O(n) Time (n=lenght*height) | O(n) Space
+function riverSizes1(matrix) {
+  const solutions = [];
+  const visited = [...Array(matrix.length)].map(() =>
+    Array(matrix[0].length).fill(false)
+  );
+  console.log(visited);
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[0].length; j++) {
+      if (visited[i][j]) continue;
+      traverseNode(i, j, matrix, visited, solutions);
+    }
+  }
+  console.log(visited);
+  console.log(solutions);
+}
+
+function traverseNode(i, j, matrix, visited, solutions) {
+  let size = 0;
+  const nodesToExpore = [[i, j]];
+  console.log(nodesToExpore);
+  while (nodesToExpore.length) {
+    const currentNode = nodesToExpore.pop();
+    const i = currentNode[0];
+    const j = currentNode[1];
+    if (visited[i][j]) continue;
+    visited[i][j] = true;
+    if (matrix[i][j] === 0) continue;
+    size++;
+    const unvisitedNeighbors = getAdjecentNodes(i, j, matrix, visited);
+    nodesToExpore.push(...unvisitedNeighbors);
+  }
+  if (size > 0) solutions.push(size);
+}
+
+function getAdjecentNodes(i, j, matrix, visited) {
+  const unvisitedNeighbors = [];
+
+  if(i<matrix.length -1 && !visited[i+1][j]) unvisitedNeighbors.push([i+1,j]);
+  if(i>0 && !visited[i-1][j]) unvisitedNeighbors.push([i-1, j]);
+  if(j<matrix[i].length -1 && !visited[i][j+1]) unvisitedNeighbors.push([i,j+1])
+  if(j>0 && !visited[i][j-1]) unvisitedNeighbors.push([i,j-1]);
+
+  return unvisitedNeighbors;
+}
+
 //Test
 
 const matrix = [
-  [
-    1, 0, 0, 1, 0
-  ],
-  [
-    1, 0, 1, 0, 0
-  ],
-  [
-    0, 0, 1, 0, 1
-  ],
-  [
-    1, 0, 1, 0, 1
-  ],
-  [
-    1, 0, 1, 1, 0
-  ]
+  [1, 0, 0, 1, 0],
+  [1, 0, 1, 0, 0],
+  [0, 0, 1, 0, 1],
+  [1, 0, 1, 0, 1],
+  [1, 0, 1, 1, 0],
 ];
 
-riverSizes(matrix);
+riverSizes1(matrix);
